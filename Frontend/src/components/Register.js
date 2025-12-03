4aad0b134c910d70e70a7e66dc7d5e9ede063490
kartikeya7609 <kartikeyakk2007@gmail.com>
2025-12-03 18:23:20 +0530

    Update Register.js

--- Frontend/src/components/Register.js
+++ Frontend/src/components/Register.js
@@ -65,7 +65,7 @@ function Register() {
 
     try {
       const response = await axios.post(
-        "https://ecommerce-1-zz8i.onrender.com/api/auth/register",
+        "https://ecommerce-1-zz8i.onrender.com/api/auth/register/",
         {
           name: formData.name,
           email: formData.email,
@@ -205,4 +205,5 @@ function Register() {
 }
 
 export default Register;
+
 
